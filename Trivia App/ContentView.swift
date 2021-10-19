c408ee38a07e46251f921dae692697eae8b44c53
philreic <91380439+philreic@users.noreply.github.com>
2021-10-19 14:54:18 -0700

    Initial Commit

---
+++ Trivia App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Trivia App
+//
+//  Created by Philippe Reichen on 10/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Trivia App/Trivia_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trivia_AppApp.swift
+//  Trivia App
+//
+//  Created by Philippe Reichen on 10/19/21.
+//
+
+import SwiftUI
+
+@main
+struct Trivia_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
